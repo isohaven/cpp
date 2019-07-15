fbcea4b8f528018b37cd93dc98a7e8c46e829c1d
isohaven <ridgehaven320@gmail.com>
2019-07-15 18:51:31 -0400

    added bubble up and private methods

--- Heap/MinHeap.cpp
+++ Heap/MinHeap.cpp
@@ -6,16 +6,33 @@ class MinHeap {
 	MinHeap() {
 	}
 	void enqueue(int value) {
-		// bubbleUp();
+		heap.push_back(value);
+		int i = heap.size() -1;
+		int parent = parent(i);
+
+		// bubble up
+		while(parent != i && list[i] < list[parent]) {
+			swap(i, parent);
+			i = parent;
+			parent = parent(i);
+		}
 	}
 	int dequeue() {
 		if(isEmpty()) {
 			throw std::runtime_error("empty queue");
 		}
 		else if (list.size() == 1) {
-			int val = heap[0];
+			int min = heap[0];
 			a.erase(a.begin());
-			return val;
+			return min;
+		}
+		else {
+			int min = heap[0];
+			int lastVal = heap.back();
+			heap.insert(0, lastVal);
+			heap.pop_back();
+			// bubbledown
+			return min;
 		}
 	}
 	int peek() {
@@ -28,6 +45,23 @@ class MinHeap {
 
 	bool isEmpty() {
 		return heap.size <= 0;
+	}
+	
+	private:
+
+	void swap(int a, int b) {
+		int temp = list[b];
+		heap.insert(b, heap[a]);
+		heap.insert(a. temp):
+	}
+	int parent(int i) {
+		return (i-1) / 2;
+	}
+	int left(int i) {
+		return 2*i + 1;
+	}
+	int right(int i) {
+		return 2*i + 2;
 	}
 
 
